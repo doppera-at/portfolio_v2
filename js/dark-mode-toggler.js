6ee275ef3bae73d66626c8e614a6a4d3315d2121
Bernhard Doppler <github@doppera.at>
2025-10-14 16:00:18 +0200

    add dark mode adjust css

---
+++ js/dark-mode-toggler.js
@@ -0,0 +1,29 @@
+// ### ON PAGE LOAD
+// Get preferences and set theme accordingly
+var localStorageTheme = localStorage.getItem("theme")
+var darkSchemePreferred = window.matchMedia("(prefers-color-scheme: dark)")
+
+if (localStorageTheme == "dark") {
+    toggleDarkMode()
+} else if (localStorageTheme == "light") {
+    toggleLightMode()
+} else {
+    localStorage.setItem("theme", darkSchemePreferred ? "dark" : "light")
+}
+
+
+function toggleTheme() {
+    if (document.body.classList.contains("dark-mode")) toggleLightMode() 
+    else toggleDarkMode()
+}
+
+function toggleLightMode() {
+    document.body.classList.remove("dark-mode")
+    document.getElementById("toggle-dark-mode").innerText = "Dark Mode"
+    localStorage.setItem("theme", "light")
+}
+function toggleDarkMode() {
+    document.body.classList.add("dark-mode")
+    document.getElementById("toggle-dark-mode").innerText = "Light Mode"
+    localStorage.setItem("theme", "dark")
+}
