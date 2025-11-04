4f434b2f87b796a8a0827b7780230c127ad1da67
Bernhard Doppler <github@doppera.at>
2025-11-04 11:44:28 +0100

    add loading of components using jQuery

---
+++ js/load-components.js
@@ -0,0 +1,3 @@
+$("header").first().load("../html/components/header.html");
+$("footer").first().load("../html/components/footer.html");
+$("nav").first().load("../html/components/navigation.html");
