f7501b0fa2aff1c5ef2508ed3e11663640253892
Bernhard Doppler <github@doppera.at>
2025-11-10 11:04:49 +0100

    add image expand modal

--- js/image-expand.js
+++ js/image-expand.js
@@ -4,10 +4,19 @@ https://www.w3schools.com/CSS/tryit.asp?filename=trycss_image_gallery_responsive
 */
 
 
-const imageModal = document.getElementById('modal-imageexpand');
-const closeModal = document.getElementById('modal-imageexpand-close');
+const modalContainer = document.getElementById('modal-container');
+const modalImage = document.getElementById('modal-content');
+const modalClose = document.getElementById('modal-close');
+// const modalCaption = document.getElementById('modal-caption');
 
-closeModal.onclick = function() {
+modalClose.onclick = function() {
     imageModal.style.display = "none";
+}
+
+
+function displayModal(img) {
+    modalContainer.style.display = "block";
+    modalImage.src = img.src;
+    modalImage.alt = img.alt;
 }
 
