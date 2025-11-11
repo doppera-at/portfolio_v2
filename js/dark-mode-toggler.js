5ba68894fd91f3ba6f7b8a13d18ee72dd722cfc3
Bernhard Doppler <github@doppera.at>
2025-11-11 03:40:10 +0100

    add style switching of highligh js themes

--- js/dark-mode-toggler.js
+++ js/dark-mode-toggler.js
@@ -1,31 +1,36 @@
 // ### ON PAGE LOAD
 // Get preferences and set theme accordingly
-var localStorageTheme = localStorage.getItem("theme")
-var darkSchemePreferred = window.matchMedia("(prefers-color-scheme: dark)")
+var localStorageTheme = localStorage.getItem("theme");
+var darkSchemePreferred = window.matchMedia("(prefers-color-scheme: dark)");
+const highlightjsCSS = document.getElementById("highlightjs-css");
+const highlightjsThemeLight = "github.min.css";
+const highlightjsThemeDark = "github-dark.min.css"
 
 if (localStorageTheme == "dark") {
-    toggleDarkMode()
+    toggleDarkMode();
 } else if (localStorageTheme == "light") {
-    toggleLightMode()
+    toggleLightMode();
 } else {
-    localStorage.setItem("theme", darkSchemePreferred ? "dark" : "light")
-    if (darkSchemePreferred) toggleDarkMode()
-    else toggleLightMode()
+    localStorage.setItem("theme", darkSchemePreferred ? "dark" : "light");
+    if (darkSchemePreferred) toggleDarkMode();
+    else toggleLightMode();
 }
 
 
 function toggleTheme() {
-    if (document.body.classList.contains("dark-mode")) toggleLightMode() 
-    else toggleDarkMode()
+    if (document.body.classList.contains("dark-mode")) toggleLightMode() ;
+    else toggleDarkMode();
 }
 
 function toggleLightMode() {
-    document.body.classList.remove("dark-mode")
-    document.getElementById("toggle-dark-mode").innerText = "Switch Theme: ☾"
-    localStorage.setItem("theme", "light")
+    document.body.classList.remove("dark-mode");
+    document.getElementById("toggle-dark-mode").innerText = "Switch Theme: ☾";
+    localStorage.setItem("theme", "light");
+    highlightjsCSS.href = `../css/highlightjs/${highlightjsThemeLight}`;
 }
 function toggleDarkMode() {
-    document.body.classList.add("dark-mode")
-    document.getElementById("toggle-dark-mode").innerText = "Switch Theme: ☀"
-    localStorage.setItem("theme", "dark")
+    document.body.classList.add("dark-mode");
+    document.getElementById("toggle-dark-mode").innerText = "Switch Theme: ☀";
+    localStorage.setItem("theme", "dark");
+    highlightjsCSS.href = `../css/highlightjs/${highlightjsThemeDark}`;
 }
